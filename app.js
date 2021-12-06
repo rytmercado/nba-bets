673790b4496e0924deee7ace47934572211c2907
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2021-12-06 10:15:27 -0800

    working local host

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express();
 const mongoose = require("mongoose")
 const db = require('./config/keys').monogURI
 const users = require("./routes/api/users")
-const tweets = require("./routes/api/tweets")
 const User = require("./models/User")
 const bodyParser = require("body-parser")
-const passport= require("./config/passport");
+const passport = require("passport");
+
+
 
 mongoose.connect(db, {  useNewUrlParser: true }).then(
   () => console.log("connected to mongoDB")
@@ -18,7 +19,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json()); 
 
-app.use(passport);
 
 
 app.get("/", (req, res) => {
@@ -30,11 +30,14 @@ app.get("/", (req, res) => {
   user.save()
 });
 
-app.use("/api/users", users)
-app.use("/api/tweets", tweets)
+// app.get("/", (req, res) => res.send("Hello World!!"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 
 const port = process.env.PORT || 5000; 
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
--- models/User.js
+++ models/User.js
@@ -18,6 +18,6 @@ const UserSchema = new Schema({
   timestamps: true
 })
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('users', UserSchema);
 
 module.exports = User;

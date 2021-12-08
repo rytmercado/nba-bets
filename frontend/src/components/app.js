decc2b947bb5568e09cf6ed6bf48a1af26fd2806
wesley <cheung.w.2016@gmail.com>
2021-12-08 15:06:20 -0800

    styled profile page and main page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch, Route } from 'react-router-dom';
+
+// components
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container'; 
 import SplashForm from './splash/splash_form'
+import ProfileContainer from './profile/profile_container'
+
+// css
 import './reset.css'
 import './splash/splash.css'
 import './nav/navbar.css'
 import './session/login_form.css'
 import './session/signup_form.css'
+import './profile/profile.css'
 
 
 const App = () => (
@@ -18,12 +24,12 @@ const App = () => (
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
         
+        <ProtectedRoute exact path="/profile" component={ProfileContainer} />
         <ProtectedRoute exact path="/main" component={MainPage} />
-        
         
         <Route path="/" component={SplashForm} />
         
-        {/* <ProtectedRoute exact path="/profile" component={ProfileContainer} /> */}
+        
     </Switch>
   </div>
 );

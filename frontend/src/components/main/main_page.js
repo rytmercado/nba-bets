7b854fc6887c3a850febf1cff48ab51efc03558e
tharpercp <tharpercp@gmail.com>
2021-12-08 08:39:04 -0800

    navbar

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NavBar from '../nav/navbar';
 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="main">
+        <NavBar/>
         <h1>NBA BETS APP</h1>
         <footer>
           Copyright &copy; 2021 NBA bets
--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -15,29 +15,30 @@ class NavBar extends React.Component {
   }
 
   // Selectively render links dependent on whether the user is logged in
-  getLinks() {
-      if (this.props.loggedIn) {
-        return (
-            <div>
-                <Link to={'/profile'}>Profile</Link>
-                <button onClick={this.logoutUser}>Logout</button>
-            </div>
-        );
-      } else {
-        return (
-            <div>
-                <Link to={'/signup'}>Signup</Link>
-                <Link to={'/login'}>Login</Link>
-            </div>
-        );
-      }
-  }
+  // getLinks() {
+  //     if (this.props.loggedIn) {
+  //       return (
+  //           <div>
+  //               <Link to={'/profile'}>Profile</Link>
+  //               <button onClick={this.logoutUser}>Logout</button>
+  //           </div>
+  //       );
+  //     } else {
+  //       return (
+  //           <div>
+  //               <Link to={'/signup'}>Signup</Link>
+  //               <Link to={'/login'}>Login</Link>
+  //           </div>
+  //       );
+  //     }
+  // }
 
   render() {
       return (
-        <div>
-            <h1>NBA Bets</h1>
-            { this.getLinks() }
+        <div className="navbar">
+            <img src="https://i.ibb.co/RzwvGf5/logo-small.jpg"/>
+            <Link to="/profile">Profile</Link>
+            <button onClick={this.logoutUser}>Logout</button>
         </div>
       );
   }

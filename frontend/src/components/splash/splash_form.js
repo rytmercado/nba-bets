5f660e67f1051451669d24f037635981aa39601c
tharpercp <tharpercp@gmail.com>
2021-12-07 16:25:15 -0800

    splash page done

--- frontend/src/components/splash/splash_form.js
+++ frontend/src/components/splash/splash_form.js
@@ -9,16 +9,18 @@ class SplashForm extends React.Component {
   render() {
     return (
       <div className="main-splash">
-        <img className="logo" src="https://i.ibb.co/0MHYw3Z/NBAA-Logo.jpg" />
-        <u style={{color: "#CBB26A" }}>Please verify your age to participate</u>
+        <img className="bg" src="https://www.nba.com/bulls/sites/bulls/files/covid-19.jpg" />
         <div className="age-buttons">
+            <img className="logo" src="https://i.ibb.co/nDm0spD/logo-medium.jpg" />
+            <strong>Please verify your age to participate</strong>
             <button id="button1"><span className="button-text">I am 21+</span></button>
-            <button id="button2"><span className="button-text">I am not 21</span></button>
+            <button id="button2"><span className="button-text2">Not 21</span></button>
             <p id="errors"></p>
+            <footer>
+              Copyright &copy; 2021 NBA bets
+            </footer>
         </div>
-        <footer>
-          Copyright &copy; 2021 NBA bets
-        </footer>
+        
       </div>
     );
   }

8071786f5dacbe4e5120d62657cceb127bde30a8
wesley <cheung.w.2016@gmail.com>
2021-12-08 12:16:16 -0800

    fixed main page call to NavBarContainer and added styling to navbar

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import NavBar from '../nav/navbar';
+import NavBarContainer from '../nav/navbar_container';
 
 class MainPage extends React.Component {
 
   render() {
     return (
       <div className="main">
-        <NavBar/>
+        <NavBarContainer/>
         <h1>NBA BETS APP</h1>
         <footer>
           Copyright &copy; 2021 NBA bets

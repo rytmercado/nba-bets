55c20204b38999834c5e8f7dfc3f48a0a780f10c
tharpercp <tharpercp@gmail.com>
2021-12-09 07:16:17 -0800

    bet modal container

---
+++ frontend/src/components/bet_modal/bet_modal_container.js
@@ -0,0 +1,14 @@
+import { connect } from 'react-redux';
+import { getGames } from '../../actions/game_actions';
+import MainPage from './main_page';
+
+
+const mSTP = state => ({
+  
+});
+
+const mDTP = dispatch => ({
+    postBet: () => dispatch(getGames()),
+  });
+
+export default connect(null, mDTP)(MainPage);

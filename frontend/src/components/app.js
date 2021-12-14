3ea9820412adf05c4062d9361d1a639bb23d7618
tharpercp <tharpercp@gmail.com>
2021-12-14 12:15:40 -0800

    styling changes

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -5,8 +5,17 @@ import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container'; 
 import SplashForm from './splash/splash_form'
+
+
 import './reset.css'
 import './splash/splash.css'
+import './nav/navbar.css'
+import './session/login_form.css'
+import './session/signup_form.css'
+import '../components/games/game_index_item.css'
+import './profile/profile.css'
+import './bet_modal/bet_modal.css'
+
 
 
 const App = () => (

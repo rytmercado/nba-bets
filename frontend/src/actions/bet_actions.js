62e5562d52e1fab518cb2470368ec6ada6a50e54
tharpercp <tharpercp@gmail.com>
2021-12-14 11:58:03 -0800

    catch errors on bet actions now

--- frontend/src/actions/bet_actions.js
+++ frontend/src/actions/bet_actions.js
@@ -2,15 +2,20 @@ import * as BetApiUtil from '../util/bet_api_util';
 import { receiveCurrentUser } from '../actions/session_actions'
 
 export const RECEIVE_BET = 'RECEIVE_BET'
-
+export const RECEIVE_BET_ERRORS = 'RECEIVE_BET_ERRORS'
 export const RECEIVE_USER_BETS = 'RECEIVE_USER_BETS'
 
-const receiveBet = (bet) => ({
+export const receiveBet = (bet) => ({
     type: RECEIVE_BET,
     bet
 })
 
-const receiveUserBets = (bets) => ({
+export const receiveErrors = errors => ({
+    type: RECEIVE_BET_ERRORS,
+    errors
+});
+
+export const receiveUserBets = (bets) => ({
     type: RECEIVE_USER_BETS,
     bets
 })
@@ -21,6 +26,9 @@ export const postBet = bet => dispatch => (
             // debugger
             dispatch(receiveBet(payload.data.bet))
             dispatch(receiveCurrentUser(payload.data.user))
+        })
+        .catch(err => {
+            dispatch(receiveErrors(err.response.data));
         })
 );
 

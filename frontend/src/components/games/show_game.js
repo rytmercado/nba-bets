5e96924a7e417c34598f59a4fb69d8f417f468a0
tharpercp <tharpercp@gmail.com>
2021-12-16 14:02:49 -0800

    nba logos rendering properly

--- frontend/src/components/games/show_game.js
+++ frontend/src/components/games/show_game.js
@@ -10,7 +10,6 @@ class ShowGame extends React.Component {
 
     componentDidMount() {
         this.props.fetchGame(this.props.match.params.id)
-        console.log(this.props.game)
     }
 
     render () {
@@ -54,15 +53,14 @@ class ShowGame extends React.Component {
             <div className="game-show">
                 <ul className="teams">
                     <li>{g.home_team}</li>
-                    <NBAIcons>
+                    {NBALogos[g.home_team]}
                     <li>{g.away_team}</li>
-                    <img className="NBAlogo" src={`/../../../../assets/photos/${g.away_team}.JPG`} />
+                    {NBALogos[g.away_team]}
                 </ul>
                 <ul className="odds">
                     <li>{g.home_odds}</li>
                     <li>{g.away_odds}</li>
                 </ul>
-
             </div>
         )
     }

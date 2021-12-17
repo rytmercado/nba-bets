790fb87718cb2bbc2ef6d056e8c5e0ce5e9c5b3f
wesley <cheung.w.2016@gmail.com>
2021-12-16 17:52:23 -0800

    got live button to hide and show

--- frontend/src/components/games/game_index_item.js
+++ frontend/src/components/games/game_index_item.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import nba_logo from '../../images/nba.png';
-import lakers_logo from '../../images/lakers.png';
-import warriors_logo from '../../images/warriors.png';
 
 import * as NBAIcons from 'react-nba-logos';
 
@@ -63,77 +61,66 @@ class GameIndexItem extends React.Component {
         } else {
             a_odds = game.away_odds;
         }
-                return (
-                        // <div className="game-index-item-grid">
-                        //     <div className="team">{game.away_team}</div>
-                        //     <div className="odds">{game.away_odds}</div>
-                        //     <div className="team">{game.home_team}</div>
-                        //     <div className="odds">{game.home_odds}</div>
-                        //     <Link to={`/game/show/${game._id}`}>Game Log</Link>
-                        // </div>
 
-                        <div className="game-index-container">
-                            <div className="game">
 
-                                <div className="game-header">
-                                    <button className="game-alert-btn">Alert</button>
-                                    <div className="nba-game-logo">
-                                        <img src={nba_logo}></img>
-                                        National Basketball League
+        return (
+                <div className="game-index-container">
+                    <div className="game">
+
+                        <div className="game-header">
+                            <button className="game-alert-btn">Alert</button>
+                            <div className="nba-game-logo">
+                                <img src={nba_logo}></img>
+                                National Basketball League
+                            </div>
+                            <Link to={`/game/show/${game._id}`}><button className="game-log-btn">View Game Log</button></Link>
+                        </div>
+                        
+                        <div className="game-content">
+                            <div className="column">
+                                <div className="team team--away">
+                                    <div className="team-logo">
+                                        {NBALogos[game.away_team]}
                                     </div>
-                                    <Link to={`/game/show/${game._id}`}><button className="game-log-btn">View Game Log</button></Link>
+                                    <h2 className="team-name">{game.away_team}</h2>
+                                    <button className="game-bet-odds">{a_odds}</button>
                                 </div>
-                                
-                                <div className="game-content">
-                                    <div className="column">
-                                        <div className="team team--away">
-                                            <div className="team-logo">
-                                                {NBALogos[game.away_team]}
-                                                {/* <img src={warriors_logo} alt="away-team"></img> */}
-                                            </div>
-                                            <h2 className="team-name">{game.away_team}</h2>
-                                            <button className="game-bet-odds">{a_odds}</button>
-                                        </div>
+                            </div>
+
+                            <div className="column">
+                                <div className="game-details">
+                                    <div className="game-period">
+                                        <strong>{game.game_time}</strong>
                                     </div>
-
-                                    <div className="column">
-                                        <div className="game-details">
-                                            <div className="game-period">
-                                                <strong>{game.game_time}</strong>
-                                            </div>
-                                            <div className="game-period">{game.game_minute}</div>
-                                            <div className="game-score">
-                                                <span className="game-score-number game-score-number--leading" >{game.away_score}</span>
-                                                <span className="game-score-divider">:</span>
-                                                <span className="game-score-number">{game.home_score}</span>
-                                            </div>
-                                            {/* <div className="game-period">
-                                                Q{game.quarter}
-                                            </div> */}
-                                            <div className="game-status">Live</div> 
-                                            <div className="game-bet">
-                                                <button className="game-bet-btn">Place Bet</button>
-                                            </div>
-                                        </div>
-
+                                    <div className="game-period">{game.game_minute}</div>
+                                    <div className="game-score">
+                                        <span className="game-score-number game-score-number--leading" >{game.away_score}</span>
+                                        <span className="game-score-divider">:</span>
+                                        <span className="game-score-number">{game.home_score}</span>
                                     </div>
-
-
-                                    <div className="column">
-                                        <div className="team team--home">
-                                            <div className="team-logo">
-                                            {NBALogos[game.home_team]}
-                                                {/* <img src={lakers_logo} alt="home-team"></img> */}
-                                            </div>
-                                            <h2 className="team-name">{game.home_team}</h2>
-                                            <button className="game-bet-odds">{h_odds}</button>
-                                        </div>
+                                    <div className={(game.away_score > 0 || game.home_score > 0 ? "game-status": "game-status-hidden")}>Live</div> 
+                                    <div className="game-bet">
+                                        <button className="game-bet-btn">Place Bet</button>
                                     </div>
+                                </div>
+
+                            </div>
+
+
+                            <div className="column">
+                                <div className="team team--home">
+                                    <div className="team-logo">
+                                    {NBALogos[game.home_team]}
+                                    </div>
+                                    <h2 className="team-name">{game.home_team}</h2>
+                                    <button className="game-bet-odds">{h_odds}</button>
                                 </div>
                             </div>
                         </div>
-                )
-            }
+                    </div>
+                </div>
+        )
+    }
 }
 
 export default GameIndexItem;

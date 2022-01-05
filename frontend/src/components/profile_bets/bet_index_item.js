456840f8e022ab57690c4a18da2354640376173c
wesley <cheung.w.2016@gmail.com>
2022-01-05 13:27:36 -0800

    fixed bet removal functionality

--- frontend/src/components/profile_bets/bet_index_item.js
+++ frontend/src/components/profile_bets/bet_index_item.js
@@ -6,24 +6,37 @@ class BetIndexItem extends React.Component{
         super(props)
 
         this.onDelete = this.onDelete.bind(this)
+        
+        this.state = {
+            status: true
+        }
     }
 
     onDelete(){
         const {bet, deleteBet} = this.props
         deleteBet(bet._id)
+        this.setState({
+            status: false
+        })
     }
     
     render(){
-        return(
-                <div className="bet-container">
-                    <span className="inline">{this.props.bet.selection}</span>
-                    <span className="inline">{this.props.bet.amount}</span>
-                    <span className="inline">{this.props.bet.payout}</span>
-                    <span className="inline">{this.props.bet.status}</span>
-                    {/* <span className="inline">{this.props.bet.createdAt}</span> */}
-                    <button className="delete-bet-btn"onClick={this.onDelete}>REMOVE BET</button>
-                </div>
-        )
+        if(this.state.status){
+            return(
+                    <div className="bet-container">
+                        <span className="inline">{this.props.bet.selection}</span>
+                        <span className="inline">{this.props.bet.amount}</span>
+                        <span className="inline">{this.props.bet.payout}</span>
+                        <span className="inline">{this.props.bet.status}</span>
+                        {/* <span className="inline">{this.props.bet.createdAt}</span> */}
+                        <button className="delete-bet-btn" onClick={this.onDelete}>REMOVE BET</button>
+                    </div>
+            )
+        } else {
+            return (
+                <div></div>
+            )
+        }
     }
 }
 

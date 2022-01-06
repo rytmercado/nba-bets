cf85595f5fa3e1587b1f2550dac5d671ac8766ad
William Winzeler <williamwinzeler@MacBook-Pro-3.local>
2022-01-06 14:11:35 -0800

    added package

---
+++ frontend/src/components/profile_graph/graph.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Graph extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  
+  render(){
+
+  }
+}
+
+export default Graph; 
---
+++ frontend/src/components/profile_graph/graph_container.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const mSTP = state => ({
+  currentUser: state.session.user
+})
